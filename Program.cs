6ea5e2f5bcc056330ec50a5d1512ce5c8336d4ae
Al Stone <astone@heckgrammar.co.uk>
2024-09-29 16:51:48 +0100

    Starting program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PseudocodeToCS;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       //This is a comment
+       int myVariable;
+       myVariable = 3;
+       const int MAXNUM = 30;
+
+    }
+}
